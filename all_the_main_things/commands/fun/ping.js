abd3101e78b14412dcbed38379c2230cda767dbc
Ly <Logitechschyters6@gmail.com>
2025-08-31 00:34:07 +0700

    Create ping.js

---
+++ all_the_main_things/commands/fun/ping.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+		category: 'fun',
+	async execute(interaction ) {
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	},
+};

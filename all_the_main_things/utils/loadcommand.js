25bbfe353ed7e3b6dbb3d23d3c077f6e62f642a3
Ly <Logitechschyters6@gmail.com>
2026-01-14 19:38:10 +0700

    load command

--- all_the_main_things/utils/loadcommand.js
+++ all_the_main_things/utils/loadcommand.js
@@ -1,29 +1,33 @@
-const fs = require('node:fs');
-const path = require('node:path');
+import fs from'node:fs';
+import path from'node:path';
+import { pathToFileURL } from 'url';
 
-function loadCommands(client) {
-    const commands = new Map();
+const __dirname = import.meta.dirname;
+
+export async function loadCommands(client) {
+    const commandsData = [];
     const foldersPath = path.join(__dirname, '../commands');
     const commandFolders = fs.readdirSync(foldersPath);
 
     for (const folder of commandFolders) {
         const commandsPath = path.join(foldersPath, folder);
+        if (!fs.lstatSync(commandsPath).isDirectory) continue;
         const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
         for (const file of commandFiles) {
             const filePath = path.join(commandsPath, file);
-            const command = require(filePath);
+            const fileUrl = pathToFileURL(filePath).href;
+            const module = await import(fileUrl);
+            const command = module.default || module;
 
             if (command.data && command.execute) {
                 client.commands.set(command.data.name, command);
-                commands.set(command.data.name, command.data.toJSON());
+                commandsData.push(command.data.toJSON());
             } else {
                 console.warn(`[WARNING] The command at ${filePath} is missing "data" or "execute".`);
             }
         }
     }
-
-    return commands;
+    return commandsData;
 }
 
-module.exports = loadCommands;
